722e9b4a02a6f919183ec7954ed669b6657b8316
Vadr0 <164426724+Vadr0@users.noreply.github.com>
2025-09-24 10:46:47 -0500

    Create Acerca.js

---
+++ SESION 06/Ejercicio 2/SRC/paginas/Acerca.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+export default function Acerca() {
+  return (
+    <div className="carta">
+      <h2>Acerca</h2>
+      <p>Esta página demuestra el enrutamiento con <code>react-router-dom</code>.</p>
+    </div>
+  )
+}
